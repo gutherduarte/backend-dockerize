839e160242bb1813c5cbfa00358fe25c3430dc44
GutherDuarte <cgdr010601@gmail.com>
2021-10-13 21:30:57 -0600

    adding API and dockerfiles

---
+++ pa_docker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pa_docker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("exito!!!!!");
+        }
+    }
+}
